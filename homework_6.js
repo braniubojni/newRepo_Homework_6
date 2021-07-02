fbe48e8c1e9535b86f694a65d67a31da01bb173f
aca_braniubojni <braniubojni@mail.ru>
2021-07-02 11:50:12 +0400

    first task done commit

---
+++ homework_6.js
@@ -0,0 +1,13 @@
+let john = {name:'John', surname: "Smith", id: 1};
+let mary = {name:'Mary', surname: "Key", id: 2};
+
+let users = [john, mary];
+
+
+let newUsers = users.map(user => {
+	user['fullName'] = `${user.name} ${user.surname}`;
+	delete user.name;
+	delete user.surname;
+	return user;
+});
+console.log(newUsers);
