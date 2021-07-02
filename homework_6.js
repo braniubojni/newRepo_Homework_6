0488d6c9d886c6ce8dea1a19044e56e90a7ff9c3
Erik Harutyunyan <braniubojni@mail.ru>
2021-07-02 12:37:48 +0400

    Update homework_6.js

--- homework_6.js
+++ homework_6.js
@@ -3,11 +3,17 @@ let mary = {name:'Mary', surname: "Key", id: 2};
 
 let users = [john, mary];
 
-
 let newUsers = users.map(user => {
-	user['fullName'] = `${user.name} ${user.surname}`;
-	delete user.name;
-	delete user.surname;
-	return user;
+	let tempUser = {};
+	tempUser['fullName'] = `${user.name} ${user.surname}`;
+	tempUser['id'] = user.id
+	return tempUser;
 });
+
+// let newUsers = users.map(user => {
+// 	user['fullName'] = `${user.name} ${user.surname}`;
+// 	delete user.name;
+// 	delete user.surname;
+// 	return user;
+// });
 console.log(newUsers);
