b3e7a82907af825ec0cef5c71b01f4db60e0e109
Erik Harutyunyan <braniubojni@mail.ru>
2021-07-03 23:07:44 +0400

    Update homework_6.js

--- homework_6.js
+++ homework_6.js
@@ -3,9 +3,7 @@ let mary = {name:'Mary', surname: "Key", id: 2};
 
 let users = [john, mary];
 
-let newUsers = users.map(user => {
-	return {fullName: `${user.name} ${user.surname}`,id:user.id};
-});
+let newUsers = users.map(user => ({fullName: `${user.name} ${user.surname}`,id:user.id}))
 
 // let newUsers = users.map(user => {
 // 	user['fullName'] = `${user.name} ${user.surname}`;
